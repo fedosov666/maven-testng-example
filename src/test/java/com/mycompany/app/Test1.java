f7049d48b3509424cdfd17e26912f2589fb3b165
Alexey Fedosov <afedosov@GDSpb1132.local>
2018-05-10 21:44:58 +0300

    Added tests

--- src/test/java/com/mycompany/app/Test1.java
+++ src/test/java/com/mycompany/app/Test1.java
@@ -1,12 +1,45 @@
 package com.mycompany.app;
 
+import io.qameta.allure.Feature;
+import io.qameta.allure.Issue;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
+@Feature("First tests")
 public class Test1 {
 
   @Test
   public void test1(){
     System.out.println("Running test 1");
+    Assert.assertTrue(true);
+  }
+
+  @Test
+  public void test2(){
+    System.out.println("Running test 2");
+  }
+
+  @Test
+  @Issue("bug")
+  public void test3(){
+    System.out.println("Running test 3");
+    Assert.assertTrue(false);
+  }
+
+  @Test
+  public void test4(){
+    System.out.println("Running test 4");
+  }
+
+  @Test
+  public void test5(){
+    System.out.println("Running test 5");
+    Assert.assertTrue(false);
+  }
+
+  @Test
+  public void test6(){
+    System.out.println("Running test 6");
   }
 
 }
--- src/test/java/com/mycompany/app/Test2.java
+++ src/test/java/com/mycompany/app/Test2.java
@@ -1,12 +1,30 @@
 package com.mycompany.app;
 
+import io.qameta.allure.Feature;
 import org.testng.annotations.Test;
 
+@Feature("Second tests")
 public class Test2 {
 
   @Test
   public void test2(){
     System.out.println("Running test 2");
   }
+
+  @Test
+  public void test1(){
+    System.out.println("Running test 1");
+  }
+
+  @Test
+  public void test3(){
+    System.out.println("Running test 3");
+  }
+
+  @Test
+  public void test4(){
+    System.out.println("Running test 4");
+  }
+
 
 }
